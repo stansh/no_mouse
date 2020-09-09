4d19796eb98b2cac21f259b58d0d621039c53d60
Stan S <stan.shm@gmail.com>
2020-09-09 16:41:24 -0500

    json linked, word longer than 10 ch not working

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-import words from 'an-array-of-english-words';
-console.log(words.length)
+/* import words from '/node_modules/an-array-of-english-words/index.js';
+console.log(words.length) */
+
+
+ let words;
+ fetch('node_modules\\an-array-of-english-words\\index.json')
+ .then(res => res.json())
+ .then(res => {
+     words = res;
+     let randomIndex = Math.floor(Math.random() * words.length);
+     let wordLength =  words[randomIndex].length;
+     if (wordLength < 10) {
+        randomIndex = Math.floor(Math.random() * words.length)
+
+     const givenWord = document.querySelector('#givenWord')
+     givenWord.innerHTML = words[randomIndex]
+     
+        })
+ .then(res => console.log(words))
+ 
