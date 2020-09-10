a3e147dc358d021d322a048c2098c9b20f4184b1
Stan S <stan.shm@gmail.com>
2020-09-09 22:37:27 -0500

    random words work, timer next

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 /* import words from '/node_modules/an-array-of-english-words/index.js';
 console.log(words.length) */
 
-
+/* window.onload = document.getElementById('input').focus(); */
+ 
+ function getRandomWord() {
  let words;
  fetch('node_modules\\an-array-of-english-words\\index.json')
  .then(res => res.json())
  .then(res => {
      words = res;
-     let randomIndex = Math.floor(Math.random() * words.length);
-     let wordLength =  words[randomIndex].length;
-     if (wordLength < 10) {
-        randomIndex = Math.floor(Math.random() * words.length)
-
+     do {
+      var randomIndex = Math.floor(Math.random() * words.length);
+     } while (words[randomIndex].length < 10)
+     console.log(words[randomIndex])
+     console.log(words[randomIndex].length)
      const givenWord = document.querySelector('#givenWord')
      givenWord.innerHTML = words[randomIndex]
-     
-        })
- .then(res => console.log(words))
- 
+     })
+
+   } 
+
+  /*  const button = document.getElementById("button");
+let mousedownTime;
+
+button.addEventListener('mousedown', () => {
+  mousedownTime = new Date().getTime();
+});
+
+button.addEventListener('mouseup', function () {
+  const mouseupTime = new Date().getTime(),
+        timeDifference = mouseupTime - mousedownTime;
+  alert(`Button held down for ${timeDifference}ms`);
+}); */
+
+   
+         
+    
